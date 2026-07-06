e760288f5d0ec01fe100a9c5f84fcf40eb8d9fae
williklassner-ui <willi.klassner@gmail.com>
2026-07-06 21:50:24 +0200

    Final clean build config - remove deprecated composeOptions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,9 +41,6 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
